f14fc650504f77692ce60e899c2d3b8201fd50a7
aleks_rdvn <aleks.rdvn@gmail.com>
2017-07-17 03:13:01 +0200

    update database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/nodekb',
+  database: 'mongodb://heroku_pzrt0zgw:286gitfeoroeul7ddq7p2t7ic7@ds161262.mlab.com:61262/heroku_pzrt0zgw',
   secret: 'yoursecret'
 }
